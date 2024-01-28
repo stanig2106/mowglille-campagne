d41923d37a3410895b2aa91e7c15bb4e67cc665e
stanig2106 <stanig2106@gmail.com>
2024-01-28 14:11:39 +0100

    test

--- app/models/cla_info.rb
+++ app/models/cla_info.rb
@@ -10,22 +10,35 @@ class ClaInfo < ApplicationRecord
   def self.create_by_token(token)
     return nil unless token
 
-    url = "https://#{CLA_HOST}/authentification/#{CLA_AUTH_IDENTIFIER}/#{token}"
+    token = URI.encode_www_form_component(token)
+
+    path = "/authentification/#{CLA_AUTH_IDENTIFIER}/#{token}"
 
     begin
-      response = Net::HTTP.get(URI(url))
+      puts "Requesting #{CLA_HOST}#{path}"
+      response = Net::HTTP.get(CLA_HOST, path)
+      puts response
 
       json = JSON.parse(response)
+      puts json
       return nil unless json['success']
 
       cla_info = ClaInfo.find_or_initialize_by(username: json['username'])
       return cla_info unless cla_info.new_record?
 
-      %w[first_name last_name school_email cursus promo]
-        .each { cla_info.send("#{_1}=", json[_1]) }
+      payload = json["payload"]
+
+      cla_info.username = payload["username"]
+      cla_info.school_email = payload["emailSchool"]
+      cla_info.first_name = payload["firstName"]
+      cla_info.last_name = payload["lastName"]
+      cla_info.cursus = payload["cursus"]
+      cla_info.promo = payload["promo"]
+
       cla_info.save!
       return cla_info
-    rescue
+    rescue StandardError => e
+      puts e
       return nil
     end
   end
