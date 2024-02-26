90bfdba8de878bc7b98ac5968a0b0656c7fcf98e
stanig2106 <stanig2106@gmail.com>
2024-02-26 17:00:48 +0100

    use cache of minio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  response.headers['Cache-Control'] = 'public, max-age=2592000'
-
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
