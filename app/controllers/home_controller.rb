be10d97407e84a45f57e11c88e439b97c95a46a0
stanig2106 <stanig2106@gmail.com>
2024-03-09 16:42:15 +0100

    fix ?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
   def index
-    render file: 'public/index.html'
+    render file: Rails.root.join('public', 'index.html')
   end
 
-
-
   def assetlinks
-    render file: 'public/assetlinks.json'
+    render file: Rails.root.join('public', 'assetlinks.json')
   end
 
   def not_found_api
